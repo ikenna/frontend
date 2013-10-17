9072e5b9ce2387dacab9efe4f2d934e539682152
Ikenna Nwaiwu <ikenna.nwaiwu@guardian.co.uk>
2013-10-17 11:00:02 +0100

    Adding Gu-Client header to requests

---
+++ discussion/test/controllers/DiscussionApiPluginTest.scala
@@ -0,0 +1,12 @@
+package controllers
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: inwaiwu
+ * Date: 16/10/13
+ * Time: 15:31
+ * To change this template use File | Settings | File Templates.
+ */
+class DiscussionApiPluginTest {
+
+}
